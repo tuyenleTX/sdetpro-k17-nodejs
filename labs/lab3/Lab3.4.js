896c39aea1fb9652902305e26eb0fc809edb3dbb
tuyenleTX <ttle@miu.edu>
2025-08-03 17:34:25 -0500

    updated merged sorted array to another way

--- labs/lab3/Lab3.4.js
+++ labs/lab3/Lab3.4.js
@@ -12,9 +12,34 @@ Expected output: [1, 1, 12, 13, 16, 16, 27, 28, 34, 99]
 //Import as a module
 const Lab3Utils = require("./Lab3Utils.js");
 
-let arr01 = [1, 12, 16, 28, 34, 0, -22];
-let arr02 = [-1, 0, 1, 13, 16, 27, 99];
-let intArr = arr01.concat(arr02);
-Lab3Utils.sortInputArray(intArr);
-console.log(intArr);
+let a = [1, 12, 16, 28, 34];
+let b = [1, 13, 16, 27, 99];
+//let intArr = a.concat(b);
+//Lab3Utils.sortInputArray(intArr);
+//console.log(intArr);
 
+let mergedArr = [];
+
+function mergeSortedArray(a, b) {
+    let i = 0; let j = 0;
+    while (i < a.length && j < b.length) {
+        if (a[i] <= b[j]) {
+            mergedArr.push(a[i]);
+            i++;
+        } else {
+            mergedArr.push(b[j]);
+            j++;
+        }
+    }
+    while (i < a.length) {
+        mergedArr.push(a[i]);
+        i++;
+    }
+    while (j < b.length) {
+        mergedArr.push(b[j]);
+        j++;
+    }
+    console.log(mergedArr);
+}
+
+mergeSortedArray(a, b);
